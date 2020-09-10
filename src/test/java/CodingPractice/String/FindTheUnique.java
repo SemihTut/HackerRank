79549fd22f6367b5b6c04dac582b2f0a98684786
SemihTut <yalaantv@gmail.com>
2020-09-10 11:41:03 +0300

    stream

--- src/test/java/CodingPractice/String/FindTheUnique.java
+++ src/test/java/CodingPractice/String/FindTheUnique.java
@@ -1,8 +1,10 @@
 package CodingPractice.String;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class FindTheUnique {
     public static void main(String[] args) {
@@ -19,6 +21,25 @@ public class FindTheUnique {
 
         }
         System.out.println("s = " + s);
+
+    }
+    @Test
+    public void stream(){
+        String str ="galatasaray";
+        List<String> asd = Arrays.asList(str.split(""));
+        asd.stream().distinct().forEach(System.out::print);
+
+
+    }
+
+    @Test
+    public void stream2(){
+
+        List<Integer> asd = Arrays.asList(2,3,5,1,9,4,3,7,3,5,1,2,9);
+        asd.stream().distinct().sorted().forEach(System.out::print);
+        List<Integer> collect = asd.stream().sorted().collect(Collectors.toList());
+        System.out.println(collect);
+
 
     }
 }
