083cffb00b78574e94d25d3484d5876f88fdf736
SemihTut <yalaantv@gmail.com>
2020-09-17 14:09:43 +0300

    map sorting by value

--- src/test/java/CodingPractice/Stream/QATestAssignment.java
+++ src/test/java/CodingPractice/Stream/QATestAssignment.java
@@ -1,14 +1,11 @@
 package CodingPractice.Stream;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class QATestAssignment {
     public static void main(String[] args) {
-
         /*
         Write a program in python or java which accepts a single list as a parameter.
         This list consists of any number of lists (1..n), and you can assume each list
@@ -21,27 +18,40 @@ public class QATestAssignment {
 
          */
 
-
-
-    }
-    public static void assignment(List<List<String>>lists){
+        List<String> listTeam = Arrays.asList("Istanbul","Ankara","Izmir","Helsinki","Espoo","Izmir","Bolu","Helsinki",
+                "Istanbul","Ankara","Izmir","Helsinki","Espoo","Izmir","Bolu","Rome","Turku","Bolu");
         //convert all lists to one list
-        List<String> stringList = lists.stream().flatMap(List::stream).collect(Collectors.toList());
+       // List<String> stringList = lists.stream().flatMap(List::stream).collect(Collectors.toList());
 
         //unique items
-        List<String> uniqueItems = stringList.stream().collect(Collectors.groupingBy(s -> s, Collectors.counting())).entrySet()
-                .stream().filter(a -> a.getValue() == 1).map(Map.Entry::getKey).collect(Collectors.toList());
+       // List<String> uniqueItems = stringList.stream().collect(Collectors.groupingBy(s -> s, Collectors.counting())).entrySet()
+       //         .stream().filter(a -> a.getValue() == 1).map(Map.Entry::getKey).collect(Collectors.toList());
 
         //number of unique items
-        int size = uniqueItems.size();
-        System.out.println("size = " + size);
+     //   int size = uniqueItems.size();
+     //   System.out.println("size = " + size);
 
         //repeated items
-        List<String> repeatedWords = stringList.stream()
-                .collect(Collectors.groupingBy(s -> s, Collectors.counting())).entrySet()
-                .stream().filter(a -> a.getValue() != 1)
-                .map(Map.Entry::getKey).collect(Collectors.toList());
-        System.out.println("frequencyWords = " + repeatedWords);
+     //   List<String> repeatedWords = stringList.stream()
+     //           .collect(Collectors.groupingBy(s -> s, Collectors.counting())).entrySet()
+    //            .stream().filter(a -> a.getValue() != 1)
+     //           .map(Map.Entry::getKey).collect(Collectors.toList());
+    //    System.out.println("repeatedWords = " + repeatedWords);
+
+        System.out.println("words that appear in more than one list");
+        LinkedHashMap<String, Long> setu = listTeam.stream().collect(Collectors.groupingBy(x -> x, Collectors.counting()))
+                .entrySet()
+                .stream().sorted(Map.Entry.comparingByValue())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+        System.out.println("setu = " + setu);
+
+
+        Map<String, Long> stringLongMap = listTeam.stream().collect(Collectors.groupingBy(s -> s, Collectors.counting()));
+        LinkedHashMap<String, Long> collect3 = stringLongMap.entrySet()
+                .stream().sorted(Map.Entry.comparingByValue())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+
+
 
     }
 }
--- src/test/java/CodingPractice/Stream/StreamUnique.java
+++ src/test/java/CodingPractice/Stream/StreamUnique.java
@@ -1,16 +1,14 @@
 package CodingPractice.Stream;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class StreamUnique {
     public static void main(String[] args) {
 
-        List<String> listTeam = Arrays.asList("Istanbul","Ankara","Izmir","Helsinki","Espoo","Izmir","Bolu","Helsinki");
+        List<String> listTeam = Arrays.asList("Istanbul","Ankara","Izmir","Helsinki","Espoo","Izmir","Bolu","Helsinki",
+                "Istanbul","Ankara","Izmir","Helsinki","Espoo","Izmir","Bolu","Rome","Turku","Bolu");
         List<String> unique = listTeam.stream()
                 .distinct().collect(Collectors.toList());//A,B,C,D,E,F
         System.out.println("unique = " + unique);
@@ -29,10 +27,44 @@ public class StreamUnique {
                 .map(StringBuilder::reverse).collect(Collectors.toList());
         System.out.println("collectReverse = " + collectReverse);
 
-        List<String> frequencyWords = listTeam.stream().collect(Collectors.groupingBy(s -> s, Collectors.counting())).entrySet()
+        List<String> frequencyWords = listTeam.stream().collect(Collectors.groupingBy(s -> s, Collectors.counting()))
+                .entrySet()
                 .stream().filter(a -> a.getValue() != 1)
                 .map(Map.Entry::getKey).collect(Collectors.toList());
         System.out.println("frequencyWords = " + frequencyWords);
+        System.out.println("----------------------------");
+        Map<String, Long> collect1 = listTeam.stream().collect(Collectors.groupingBy(s -> s, Collectors.counting()));
+        System.out.println("collect1.keySet() = " + collect1.keySet());
+        System.out.println("collect1 = " + collect1);
+        Map<String, Long> collect2 = listTeam.stream().collect(Collectors.groupingBy(s -> s.substring(0,3), Collectors.counting()));
+        System.out.println("collect2 = " + collect2);
+        System.out.println("----------------------------");
+        Map<String, Long> stringLongMap = listTeam.stream().collect(Collectors.groupingBy(s -> s, Collectors.counting()));
+        LinkedHashMap<String, Long> collect3 = stringLongMap.entrySet()
+                .stream().sorted(Map.Entry.comparingByValue())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+        System.out.println("collect3 = " + collect3);
+        String key = collect3.entrySet().stream().max((entry1, entry2) -> entry1.getValue() > entry2.getValue() ? 1 : -1).get().getKey();
+        System.out.println("key = " + key);
+        System.out.println("----------------------");
+        String key1 = listTeam.stream().collect(Collectors.groupingBy(s -> s, Collectors.counting()))
+                .entrySet().stream().max((a1, a2) -> a1.getValue() > a2.getValue() ? 1 : -1).get().getKey();
+        System.out.println("key1 = " + key1);
+
+        //SECOND WAY TO FIND KEY THAT IS THE BIGGEST VALUE
+        String key2 = listTeam.stream().collect(Collectors.groupingBy(s -> s, Collectors.counting()))
+                .entrySet().stream().min(Comparator.comparingLong(Map.Entry::getValue))
+                .get().getKey();
+        System.out.println("min = " + key2);
+
+
+        listTeam.stream()
+                .collect(Collectors.groupingBy(s -> s, Collectors.counting()))
+
+                .entrySet().stream().filter(a -> a.getValue()!=1)
+                ;
+
 
     }
 }
+
