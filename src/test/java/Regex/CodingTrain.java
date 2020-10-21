47787f6d2eadca7b68611015dc2bebaf1ff52ad7
SemihTut <yalaantv@gmail.com>
2020-10-21 19:23:14 +0300

    regex

--- src/test/java/Regex/CodingTrain.java
+++ src/test/java/Regex/CodingTrain.java
@@ -9,16 +9,18 @@ public class CodingTrain {
     public static void main(String[] args) {
 
         String text = "Galatasaray, 22 sampiyonlugunu aldiginda ben 33 yasindaydim" +
-                " asdli gelir mi asd@gmail.com 236-568, asli , asli23, assli345 semi";
+                " asdli gelir (mi) asd1@gmail.com 236-568, (asli) , asli23, assli345 semi sem.tit@gmail.com";
         String reg = "\\d{2}";
         String reg1 = "[^\\d\\s]";
         String reg2 = "\\W";
         String reg3 = "asd?li\\s";
         String reg4 = "as\\w?li\\d{2}";
         String reg5 = "\\b\\w{4}\\b"; //4 harfli kelimler
+        String reg6 = "\\(\\b\\w+\\)";
+        String reg7 = "[\\w.]+@\\w+[.](com|net)+";
         System.out.println("reg(text,reg) = " + reg(text, reg5));
-        System.out.println("reg1(text,reg1) = " + reg(text, reg1));
-        System.out.println("reg1(text,reg2) = " + reg(text, reg2));
+        System.out.println("reg1(text,reg6) = " + reg(text, reg6));
+        System.out.println("reg1(text,reg7) = " + reg(text, reg7));
 
 
     }
