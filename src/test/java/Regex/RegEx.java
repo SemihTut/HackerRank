e51c662580badf973c0d6b30a555add59bebe22f
SemihTut <yalaantv@gmail.com>
2020-10-24 14:13:13 +0300

    regex ex

--- src/test/java/Regex/RegEx.java
+++ src/test/java/Regex/RegEx.java
@@ -10,9 +10,21 @@ public class RegEx {
 
     public static void main(String[] args) {
 
-        String result = "1-48 of 135 results for 'selenium' semihtut123@gmail.com sem_ih@hotmail.edu";
+        String result = "1-48 of 135 results for \"selenium\" semihtut123@gmail.com sem_ih@hotmail.edu";
         String regNumber = "[a-zA-Z\\s\\W+]";
         String regSearch = "[\\w.]+@\\w+\\.(net|com|edu)";
+        String resultNumber = "\\s\\d+\\s";
+        String pageNumber = "\\d-?\\d+";
+        String searchTerm = "\"\\w+\"";
+        String searchTerm2 = "[\"\\w+$\"]";
+        System.out.println("converter(result,searchTerm) = " + converter(result, searchTerm2));
+        System.out.println("converter(result,pageNumber) = " + converter(result, pageNumber));
+
+        Matcher resultMatcher = Pattern.compile(resultNumber).matcher(result);
+        if(resultMatcher.find()){
+            String group = resultMatcher.group().trim();
+            System.out.println("group = " + group);
+        }
         Matcher m = Pattern.compile(regSearch).matcher(result);
         int count =0;
         List<String> emails = new ArrayList<>();
@@ -28,11 +40,20 @@ public class RegEx {
 
 
         String manpltd =  result.replaceAll(regNumber,"");
-        String searchTerm = result.replaceAll(regSearch,"1");
+        //String searchTerm = result.replaceAll(regSearch,"1");
 
-        System.out.println("searchTerm = " + searchTerm);
+
 
         System.out.println("manpltd = " + manpltd);
 
+    }
+    public static String converter(String str, String regex){
+        Matcher resultMatcher = Pattern.compile(regex).matcher(str);
+        String group = "";
+        if(resultMatcher.find()){
+            group += resultMatcher.group().trim();
+            //System.out.println("group = " + group);
+        }
+        return group;
     }
 }
