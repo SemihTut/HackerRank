35d62d5db3a82d068860f18d0d4e93065bf76388
SemihTut <yalaantv@gmail.com>
2020-11-06 18:06:33 +0200

    hashmap

--- src/test/java/CodingPractice/Stream/ternary.java
+++ src/test/java/CodingPractice/Stream/ternary.java
@@ -1,13 +1,18 @@
 package CodingPractice.Stream;
 
+import org.junit.jupiter.api.Test;
+
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class ternary {
     public static void main(String[] args) {
         int i = algoQ(9753, 8641);
         System.out.println("i = " + i);
+        deneme();
     }
     public static int algoQ(int a, int b) {
         String first = String.valueOf(a);
@@ -27,6 +32,16 @@ public class ternary {
                     Math.abs(newListSecond.get(i) + 10 - arrActual2.get(i))
                     : Math.abs(arrActual2.get(i) - newListSecond.get(i));
         }
+
         return count;
+    }
+
+    public static void deneme(){
+        Map<String,Integer> myMap = new HashMap<>();
+        myMap.put("Semih",35);
+        myMap.put("Asli",30);
+        myMap.forEach((s,t)-> System.out.println(s+"  "+t));
+
+
     }
 }
