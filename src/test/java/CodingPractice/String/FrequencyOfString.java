b740c95c45448684629c27977f6b63911108474b
SemihTut <yalaantv@gmail.com>
2020-11-12 20:09:32 +0200

    uniq&stream

--- src/test/java/CodingPractice/String/FrequencyOfString.java
+++ src/test/java/CodingPractice/String/FrequencyOfString.java
@@ -1,12 +1,15 @@
 package CodingPractice.String;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class FrequencyOfString {
 
     public static void main(String[] args) {
         List<String> names = new ArrayList<>(Arrays.asList("Semih","Yahya","Hulya","Asli","Ahmet","Yahya","Ahmet"));
         System.out.println("freq(names) = " + freq(names));
+        System.out.println("freq2(names) = " + freq2(names));
+        System.out.println("UNIQ = " + uniq(names));
 
     }
 
@@ -19,13 +22,22 @@ public class FrequencyOfString {
         return nameAndFreq;
     }
 
-    public static Map<String,Integer> freq2(List<String> name){
+    public static List<String> freq2(List<String> name){
         Map<String,Integer> nameAndFreq = new HashMap<>();
         for (String s : name){
             nameAndFreq.merge(s,1,Integer::sum);
         }
-        nameAndFreq.entrySet().stream()
-                .filter()
+        return nameAndFreq.entrySet().stream()
+                .map(x->x.getValue()+x.getKey()).collect(Collectors.toList());
+    }
+    public static List<String> uniq(List<String> name){
+        Map<String,Integer> nameAndFreq = new HashMap<>();
+        for (String s : name){
+            nameAndFreq.merge(s,1,Integer::sum);
+        }
+        return nameAndFreq.entrySet().stream()
+                .filter(x->x.getValue()==1)
+                .map(Map.Entry::getKey).collect(Collectors.toList());
     }
 
 }
