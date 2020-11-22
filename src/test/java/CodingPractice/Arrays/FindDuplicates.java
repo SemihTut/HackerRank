2293fbbd9ed2fa402a879ca668cef1f16e9b6880
SemihTut <yalaantv@gmail.com>
2020-11-22 20:32:20 +0200

    stream and merge

--- src/test/java/CodingPractice/Arrays/FindDuplicates.java
+++ src/test/java/CodingPractice/Arrays/FindDuplicates.java
@@ -1,12 +1,13 @@
 package CodingPractice.Arrays;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class FindDuplicates {
     public static void main(String[] args) {
         List<Integer> arr = new ArrayList<>(Arrays.asList(2,3,45,3,67,34,2,3,45));
         Map<Integer,Integer> quantity = new HashMap<>();
-        //first way
+        //first way merge and for each
         for(Integer a : arr){
             quantity.merge(a,1,Integer::sum);
         }
@@ -19,5 +20,14 @@ public class FindDuplicates {
         }
         System.out.println("end = " + end);
         System.out.println("end2 = " + end2);
+
+        //second way using stream
+        List<Integer> collect = quantity.entrySet().stream()
+                .filter(x -> x.getValue() != 1)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+        System.out.println("collect = " + collect);
+
+
     }
 }
