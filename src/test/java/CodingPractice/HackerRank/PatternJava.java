16178d82aafff6d25b3417d3e1c9f88d692ed0fd
SemihTut <yalaantv@gmail.com>
2020-11-29 00:11:29 +0200

    pattern in Java exercises

---
+++ src/test/java/CodingPractice/HackerRank/PatternJava.java
@@ -0,0 +1,75 @@
+package CodingPractice.HackerRank;
+
+public class PatternJava {
+
+    public static void main(String[] args) {
+       /* rightTriangle(4);
+        leftTriangle(4);
+        pyramid(4);*/
+        pyramidBack(4);
+
+    }
+
+    public static void rightTriangle(int n) {
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void leftTriangle(int n){
+        int i, j;
+        for (i=0; i<n; i++){
+            for (j=2*(n-i); j>=0; j--){
+                System.out.print(" ");
+            }
+            for (j=0; j<=i; j++ ) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pyramid(int n){
+
+        int i,j,k;
+        for(i=0;i<n;i++){
+            for(j=n-i;j>0;j--){
+                System.out.print(" ");
+            }
+            for (j=0; j<=i; j++ ) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+
+    }
+    public static void pyramidBack(int n){
+
+        int i,j,k,t,y;
+        for(i=0;i<n;i++){
+            for(j=n-i;j>0;j--){
+                System.out.print(" ");
+            }
+            for (j=0; j<=i; j++ ) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+        for(k=1;k<n;k++){
+            for(t=0;t<k;t++){
+                System.out.print(" ");
+
+            }
+            for(y=0;y<=n-1-k;y++){
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+    }
+}
