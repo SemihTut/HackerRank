80080762fdd662ee6390f460e52caa0ae3106e22
SemihTut <yalaantv@gmail.com>
2020-12-14 17:15:16 +0200

    stream&array&biggest number

---
+++ src/test/java/CodingPractice/HackerRank/TheHurdleRace.java
@@ -0,0 +1,20 @@
+package CodingPractice.HackerRank;
+
+import java.util.Arrays;
+import java.util.OptionalInt;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class TheHurdleRace {
+    public static void main(String[] args) {
+        int[]list = {1,6,3,5,2};
+        System.out.println("hurdleRace(7,list) = " + hurdleRace(4, list));
+
+    }
+    static int hurdleRace(int k, int[] height) {
+
+        int max = Arrays.stream(height).max().getAsInt();
+        return max>k?(max-k):0;
+
+    }
+}
