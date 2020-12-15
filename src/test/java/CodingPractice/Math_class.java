03e991ce858d539d7e707c57b7dd3e9465a2f995
SemihTut <yalaantv@gmail.com>
2020-12-15 22:49:13 +0200

    Math.class

---
+++ src/test/java/CodingPractice/Math_class.java
@@ -0,0 +1,11 @@
+package CodingPractice;
+
+public class Math_class {
+    public static void main(String[] args) {
+        int i = Math.addExact(10, 15);
+        System.out.println("Math.ceil(2.7) = " + Math.ceil(2.7));
+        System.out.println("Math.floor(2.7) = " + Math.floor(2.7));
+        System.out.println("Math.floorMod(10,4) = " + Math.floorMod(10, 3));
+        System.out.println("i = " + i);
+    }
+}
