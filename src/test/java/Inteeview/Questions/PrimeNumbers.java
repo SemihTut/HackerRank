027428ceade16af5805645f1ed705618dbf7e06e
SemihTut <yalaantv@gmail.com>
2020-12-24 22:20:18 +0200

    upd

--- src/test/java/Inteeview/Questions/PrimeNumbers.java
+++ src/test/java/Inteeview/Questions/PrimeNumbers.java
@@ -1,17 +1,31 @@
 package Inteeview.Questions;
 
+import java.util.*;
+
 public class PrimeNumbers {
     public static void main(String[] args) {
-        for(int i=3;i<100;i++){
-            boolean isPrime = true;
-            for(int j=2; j<Math.sqrt(i);j++){
-                if(i%j==0){
-                    isPrime = false;
-                    break;
-                }
-            }
+        System.out.println("prList(10) = " + prList(119));
 
+    }
+    private static boolean isPrime(int n){
+        for(int i=2;i<=Math.sqrt(n);i++){
+            if(n%i==0){
+                return false;
+            }
         }
+        return true;
+    }
+    private static List<Integer>prList(int n){
+        List<Integer>primeList=new ArrayList();
+        if(n>=2){
+           primeList.add(2);
+        }
+        for(int i=3;i<n;i+=2){
+            if(isPrime(i)){
+                primeList.add(i);
+            }
+        }
+        return primeList;
     }
 
 }
