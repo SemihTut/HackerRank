f960817d8662a9cbf2a29caa5c9b9ef5dfbd4fdf
SemihTut <yalaantv@gmail.com>
2021-01-08 23:28:55 +0200

    updated

--- src/test/java/CodingPractice/CodeReview/NumberLineJumps.java
+++ src/test/java/CodingPractice/CodeReview/NumberLineJumps.java
@@ -8,4 +8,14 @@ public class NumberLineJumps {
 
         return v1<=v2?"NO":(x2-x1)%(v1-v2)!=0?"NO":"YES";
     }
+
+    private static String jumps2(int x1,int v1, int x2,int v2){
+
+        return v1<=v2?"NO":(x2-x1)%(v1-v2)!=0?"NO":"YES";
+    }
+
+    private static String jumps3(int x1,int v1, int x2,int v2){
+
+        return v1<=v2?"NO":(x2-x1)%(v1-v2)!=0?"NO":"YES";
+    }
 }
