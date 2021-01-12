b31a68c6accf85bb314acdb453972d70dfecc23f
SemihTut <yalaantv@gmail.com>
2021-01-12 23:34:17 +0200

    status

---
+++ src/test/java/CodingPractice/Arrays/Arithmatic.java
@@ -0,0 +1,12 @@
+package CodingPractice.Arrays;
+
+import java.util.Scanner;
+
+public class Arithmatic {
+    public static void main(String[] args) {
+        System.out.println(arth(2, 5));
+    }
+    private static double arth(double a, double b){
+        return (a+b)/2;
+    }
+}
