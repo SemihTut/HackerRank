6b9ac5dcb93cc027736ef9f4da83a7694477f4a1
SemihTut <yalaantv@gmail.com>
2021-02-07 15:41:07 +0200

    update

--- src/test/java/CodingPractice/freeTimeStudy.java
+++ src/test/java/CodingPractice/freeTimeStudy.java
@@ -1,11 +1,13 @@
 package CodingPractice;
 
+import java.util.*;
 import java.util.stream.IntStream;
 
 public class freeTimeStudy {
 
     public static void main(String[] args) {
         System.out.println("robob = " + isPalindrome("ey edip adanada pide ye"));
+        System.out.println(compareStrings("abcde", "bcdef"));
     }
 
     public static boolean isPalindrome(String str){
@@ -17,6 +19,18 @@ public class freeTimeStudy {
 
         // str.replaceAll("\\s,"").toLowerCase();
         // str.replaceAll("\\s,"").toLowerCase();
+
+    }
+
+    public static double compareStrings(String first, String second){
+        List<String>firstOne = new ArrayList<>(Arrays.asList(first.split("")));
+        List<String>secondOne = new ArrayList<>(Arrays.asList(second.split("")));
+        double size = firstOne.size();
+        for(String a : firstOne){
+            secondOne.remove(a);
+        }
+        double unique = secondOne.size();
+        return unique/size;
 
     }
 }
