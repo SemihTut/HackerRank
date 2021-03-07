ac26a42617df7aa3369131eccaab512511b97f8f
SemihTut <yalaantv@gmail.com>
2021-03-08 00:46:42 +0200

    duplicates

--- src/test/java/CodingPractice/Arrays/FindDuplicates.java
+++ src/test/java/CodingPractice/Arrays/FindDuplicates.java
@@ -5,18 +5,21 @@ import java.util.stream.Collectors;
 
 public class FindDuplicates {
     public static void main(String[] args) {
-        List<Integer> arr = new ArrayList<>(Arrays.asList(2,3,45,3,67,34,2,3,45,67,9876));
-        Map<Integer,Integer> quantity = new HashMap<>();
+
+        String sss = "aabbcsdfjkhsakjdghlöadkfjsgöoaierugöoaksndfgölasnmgfoöaiwjgfölasmdfölaskjöofgkndga";
+        System.out.println("newOne(sss) = " + newOne(sss));
+        List<Integer> arr = new ArrayList<>(Arrays.asList(2, 3, 45, 3, 67, 34, 2, 3, 45, 67, 9876));
+        Map<Integer, Integer> quantity = new HashMap<>();
         //first way merge and for each
-        for(Integer a : arr){
-            quantity.merge(a,1,Integer::sum);
+        for (Integer a : arr) {
+            quantity.merge(a, 1, Integer::sum);
         }
         System.out.println(quantity);
         List<Integer> end = new ArrayList<>();
         List<Integer> end2 = new ArrayList<>();
 
-        for(Integer i : quantity.keySet()){
-            (quantity.get(i)>1?end:end2).add(i);
+        for (Integer i : quantity.keySet()) {
+            (quantity.get(i) > 1 ? end : end2).add(i);
         }
         System.out.println("end = " + end);
         System.out.println("end2 = " + end2);
@@ -29,5 +32,30 @@ public class FindDuplicates {
         System.out.println("collect = " + collect);
 
 
+    }
+
+    public static String newOne(String str) {
+        List<String> myList = Arrays.asList(str.split(""));
+        String result = "";
+        for (String s : myList) {
+            int quant = quantity(str, s);
+            if (result.contains(s)) {
+                result = result;
+            } else {
+                result += quant + s;
+            }
+        }
+        return result;
+
+    }
+
+    public static int quantity(String str, String c) {
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == c.charAt(0))
+                count++;
+        }
+        return count;
     }
 }
