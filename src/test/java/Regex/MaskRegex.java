73f47eb3956b8dc68aedb2aa454a39f121082353
SemihTut <yalaantv@gmail.com>
2021-03-07 13:50:03 +0200

    regex

--- src/test/java/Regex/MaskRegex.java
+++ src/test/java/Regex/MaskRegex.java
@@ -2,12 +2,14 @@ package Regex;
 
 public class MaskRegex {
     public static void main(String[] args) {
-        System.out.println(maskele("semih naber adamim"));
+        System.out.println(mask("ILoveAutomation!"));
 
     }
 
-    public static String maskele(String str){
+    public static String mask(String str){
 
-        return str.replaceAll(".(?<=.{5})", "*");
+        return str.replaceAll(".(?<=.{6})", "*");
+        //Input  = ILoveAutomation!
+        //Output = ILove***********
     }
 }
